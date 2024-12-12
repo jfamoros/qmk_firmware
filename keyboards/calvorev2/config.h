4638928bf44a3f310928c37552a0b2a4d66e049c
Jordi Amoros <jfamoros80@gmail.com>
2024-12-12 17:17:22 +0100

    add encoders

--- keyboards/calvorev2/config.h
+++ keyboards/calvorev2/config.h
@@ -13,3 +13,12 @@
 //#define SERIAL_USART_PIN_SWAP    // Swap TX and RX pins if keyboard is master halve.
 //                                   // Check if this feature is necessary with your keyboard design and available on the mcu.
 #define SERIAL_PIO_USE_PIO1 // Force the usage of PIO1 peripheral, by default the Serial implementation uses the PIO0 peripheral
+
+#define ENCODER_A_PINS { GP14 }
+#define ENCODER_B_PINS { GP15 }
+#define ENCODER_RESOLUTIONS { 4 }
+#define ENCODER_A_PINS_RIGHT { GP14 }
+#define ENCODER_B_PINS_RIGHT { GP15 }
+#define ENCODER_RESOLUTIONS_RIGHT { 4 }
+
+#define ENCODER_DEFAULT_POS 0x3
